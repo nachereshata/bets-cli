bd5ff6d311a041c930ad9530f9f1b48bc1242864
nachereshata <nachereshata@gmail.com>
2019-01-27 04:15:41 +0200

    added tests for match.parse_file
    Signed-off-by: nachereshata <nachereshata@gmail.com>

--- tests/model/test_match.py
+++ tests/model/test_match.py
@@ -1,4 +1,8 @@
 import pytest
+import tempfile
+
+from os import path
+from os import unlink
 
 from bets.model import match
 
@@ -175,3 +179,52 @@ def test_parse_text_raises_value_error_if_no_matches_in_text():
         match.parse_text("""
         no matches here too
         """)
+
+
+def test_parse_file_raises_type_error_if_not_string():
+    with pytest.raises(TypeError):
+        match.parse_file(1)
+    with pytest.raises(TypeError):
+        match.parse_file(None)
+    with pytest.raises(TypeError):
+        match.parse_file([])
+    with pytest.raises(TypeError):
+        match.parse_file(object())
+
+
+def test_parse_file_raises_value_error_if_empty_string():
+    with pytest.raises(ValueError):
+        match.parse_file("")
+
+
+def test_parse_file_returns_list_of_matches_when_in_file():
+    text = """
+        Barcelona - Liverpool 2.34 3.40 2.5
+        2134
+        Man utd. - Arsenal 2.78 3.9 3.5
+        123
+        """
+
+    file_path = path.join(tempfile.gettempdir(), "matches.txt")
+
+    with open(file_path, "wb") as fp:
+        fp.write(text.encode("utf-8"))
+
+    _matches = match.parse_file(file_path)
+
+    unlink(file_path)
+
+    assert isinstance(_matches, list)
+    assert len(_matches) == 2
+
+    for _match in _matches:
+        assert match.is_match(_match)
+
+    assert _matches[0][IDX_TITLE] == "Barcelona - Liverpool"
+    assert _matches[0][IDX_1] == 2.34
+    assert _matches[0][IDX_X] == 3.40
+    assert _matches[0][IDX_2] == 2.5
+    assert _matches[1][IDX_TITLE] == "Man utd. - Arsenal"
+    assert _matches[1][IDX_1] == 2.78
+    assert _matches[1][IDX_X] == 3.90
+    assert _matches[1][IDX_2] == 3.50
