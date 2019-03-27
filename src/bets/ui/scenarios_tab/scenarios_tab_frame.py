69dd52cb5bbb5443f83b4784a598cc4196dd7e76
nachereshata <nachereshata@gmail.com>
2019-03-27 07:52:38 +0200

    cleanup in scenarios_tab_frame

--- src/bets/ui/scenarios_tab/scenarios_tab_frame.py
+++ src/bets/ui/scenarios_tab/scenarios_tab_frame.py
@@ -1,8 +1,9 @@
-from tkinter import Button, Frame, IntVar, Label, LabelFrame
+from tkinter import Button, Frame, IntVar, Label, LabelFrame, EW
 from typing import List
 
 from bets.model.matches import Matches
 from bets.model.scenarios import Scenarios
+from bets.ui.constants import PAD_X, PAD_Y
 from bets.ui.scenarios_tab.scenarios_data_row import ScenariosDataRow
 
 
@@ -19,7 +20,7 @@ class ScenariosTabFrame(Frame):
         self.var_scenarios_count = IntVar()
         self.var_scenarios_count.set(0)
         self.gen_frame = LabelFrame(self, text=" Initial data ")
-        self.gen_frame.grid(column=0, row=0, padx=4, pady=2, )
+        self.gen_frame.grid(column=0, row=0, padx=PAD_X, pady=PAD_Y)
         self.create_widgets()
         self.scenarios_rows: List[ScenariosDataRow] = []
 
@@ -37,11 +38,11 @@ class ScenariosTabFrame(Frame):
 
     def add_scenarios_row(self, title: str, scenarios: Scenarios):
         row = ScenariosDataRow(self, title, scenarios)
-        row.grid(column=0, row=(len(self.scenarios_rows) + 1), padx=4, pady=2, sticky="WE")
+        row.grid(column=0, row=(len(self.scenarios_rows) + 1), padx=PAD_X, pady=PAD_Y, sticky=EW)
         self.scenarios_rows.append(row)
 
     def create_widgets(self):
         gen_btn = Button(self.gen_frame, text="(Re)Generate Scenarios", command=self._generate_scenarios)
-        gen_btn.grid(column=0, row=0, sticky="WE", padx=4, pady=2)
+        gen_btn.grid(column=0, row=0, sticky=EW, padx=PAD_X, pady=PAD_Y)
         Label(self.gen_frame, text="Total scenarios count:").grid(column=1, row=0, padx=10, pady=5)
         Label(self.gen_frame, textvariable=self.var_scenarios_count).grid(column=2, row=0, padx=10, pady=5)
