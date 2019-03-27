00eef372b0eaf2aa799f7f37606307f593f63dcb
nachereshata <nachereshata@gmail.com>
2019-03-27 07:45:36 +0200

    small cleanup in matches frrame

--- src/bets/ui/matches_tab/matches_table_frame.py
+++ src/bets/ui/matches_tab/matches_table_frame.py
@@ -1,6 +1,7 @@
-from tkinter import StringVar
+from tkinter import StringVar, NSEW
 from tkinter.ttk import Label, LabelFrame
 
+from bets.ui.constants import PAD_X, PAD_Y
 from bets.ui.matches_observable import MatchesObserver, MatchesObservable
 
 
@@ -8,10 +9,10 @@ class MatchesTableFrame(LabelFrame, MatchesObserver):
 
     def __init__(self, parent, matches: MatchesObservable, column=0, row=2):
         super().__init__(parent, text=" Matches ")
-        self.grid(column=column, row=row, columnspan=2, padx=4, pady=2, sticky="WNES")
+        self.grid(column=column, row=row, columnspan=2, padx=PAD_X, pady=PAD_Y, sticky=NSEW)
         self.var_table = StringVar()
         self.table_label = Label(self, textvariable=self.var_table, font="Consolas")
-        self.table_label.grid(column=0, row=0, padx=4, pady=2, sticky="WNES")
+        self.table_label.grid(column=0, row=0, padx=PAD_X, pady=PAD_Y, sticky=NSEW)
         matches.add_observer(self)
 
     def matches_changed(self, matches_observable: MatchesObservable):
