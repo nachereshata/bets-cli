42bcdb16c513c262db1cee4ba9050582f405d983
nachereshata <nachereshata@gmail.com>
2019-03-27 07:05:37 +0200

    refactored matches_tab

--- src/bets/ui/matches_tab/matches_tab.py
+++ src/bets/ui/matches_tab/matches_tab.py
@@ -1,13 +1,13 @@
 import tkinter as tk
 
-from bets.ui.matches_tab.matches_table import MatchesTable
+from bets.ui.matches_tab.manual_match_input_frame import ManualMatchInputFrame
+from bets.ui.matches_tab.matches_table_frame import MatchesTableFrame
 from bets.ui.matches_tab.pasted_matches_input import PastedMatchesInput
-from bets.ui.matches_tab.single_match_input import SingleMatchInput
 
 
 class MatchesTab(tk.Frame):
-    matches_table: MatchesTable
-    single_match_input: SingleMatchInput
+    matches_table: MatchesTableFrame
+    single_match_input: ManualMatchInputFrame
     pasted_matches_input: PastedMatchesInput
 
     def __init__(self, win, tabs, matches):
@@ -19,12 +19,6 @@ class MatchesTab(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        # create matches table
-        self.matches_table = MatchesTable(self)
-        self.matches.add_observer(self.matches_table)
-        self.matches_table.matches_changed(self.matches)
-        # single match input
-        self.single_match_input = SingleMatchInput(self, self.matches)
-
-        # pasted match input
-        self.pasted_matches_input = PastedMatchesInput(self, self.matches)
+        self.pasted_matches_input = PastedMatchesInput(self, self.matches, column=0, row=0)
+        self.single_match_input = ManualMatchInputFrame(self, self.matches, column=1, row=0)
+        self.matches_table = MatchesTableFrame(self, self.matches, column=0, row=2)
